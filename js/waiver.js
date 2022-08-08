a2ceac198d20335e4acd7010519479ffe0162694
sxu103-su <scxu@umich.edu>
2022-08-07 20:45:49 -0400

    com

---
+++ js/waiver.js
@@ -0,0 +1,48 @@
+
+  //原生js实现
+var fg = document.getElementsByTagName('figure');
+var fgl= fg.length
+for(var i=0; i<fgl; i++){
+    fg[i].className = "one-third";
+}
+
+
+var ft = document.getElementsByTagName('footer')[0];
+
+ft.addEventListener('click', () => {
+  ft.style.display = 'none';
+});
+
+
+var five = document.getElementsByTagName('figure')[4];
+
+five.addEventListener('dblclick', () => {
+  five.style.visibility = "hidden";
+});
+
+var first = document.getElementsByTagName('figure')[0];
+first.addEventListener("mouseover", (event) => {
+    event.target.style.fontFamily = "Cursive";
+    
+}, false);
+
+first.addEventListener("mouseout", (event) => {
+    event.target.style.fontFamily = "Times";
+    
+}, false);
+
+var fst= first.getElementsByTagName('figcaption')[0];
+fst.tabIndex = "1";
+fst.addEventListener("focus", (event) => {
+    event.target.style.fontFamily = "Cursive";
+    
+}, false);
+
+fst.addEventListener("blur", (event) => {
+    event.target.style.fontFamily = "Times";
+    
+}, false);
+
+first.getElementsByTagName('figcaption')[0].addEventListener("tab", () => {
+  document.getElementById("myTextField").focus();
+});
